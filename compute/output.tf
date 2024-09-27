ba6ceffe168f75a7d3aaacb901865a7fe09de706
Pritam1705 <kondapratiwarpritam855@gmail.com>
2024-09-27 14:10:23 +0530

    Create output.tf

---
+++ compute/output.tf
@@ -0,0 +1,24 @@
+output "private01_vm_id" {
+  value = aws_instance.server-01.id
+}
+
+output "private02_vm_id" {
+  value = aws_instance.server-02.id
+}
+
+output "public01_id" {
+  value = aws_instance.web.id
+}
+
+# target grp output
+
+output "Target_group_arn" {
+  value = aws_lb_target_group.test.arn
+}
+
+
+# Application LB output
+
+output "ALB_arn" {
+  value = aws_lb.Grafana-alb.arn
+}
